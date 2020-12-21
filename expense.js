0830bf359f2e32be9a4f056e2a73c0e82cbbd0b8
Grace Castaneda <griseldacastaneda2013@gmail.com>
2020-12-20 16:32:01 -0800

    refactored table and row tags

--- expense.js
+++ expense.js
@@ -1,69 +1,76 @@
-const input = document.getElementById("input");
-const tableTransaction = document.getElementById("transaction-table");
-
 const submitBtn = document.getElementById("btn");
-submitBtn.addEventListener("click", addTransaction);
+submitBtn.addEventListener("click", createExpense);
 
-function addTransaction(event) {
+function createExpense(event) {
   event.preventDefault();
 
-  const type = document.getElementById("type");
+  const type = document.getElementById("type").value;
+  const name = document.getElementById("name").value;
+  const date = document.getElementById("date").value;
+  const amount = document.getElementById("amount").value;
 
-  const name = document.getElementById("name");
+  const expense = { type, name, date, amount };
 
-  const date = document.getElementById("date");
-
-  const amount = document.getElementById("amount");
-
-  addRow(type.value, name.value, date.value, amount.value);
+  renderExpense(expense);
 }
 
-function addRow(type, category, date, amount) {
-  const formattedDate = new Intl.DateTimeFormat("en-US").format(
-    new Date(date.replace(/-/g, "/"))
+function renderExpense(expense) {
+  const table = document.getElementById("transaction-table");
+
+  expense.date = new Intl.DateTimeFormat("en-US").format(
+    new Date(expense.date.replace(/-/g, "/"))
   );
 
-  let row = tableTransaction.insertRow(-1);
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-  let cell3 = row.insertCell(2);
-  let cell4 = row.insertCell(3);
-  let cell5 = row.insertCell(4);
+  const expenseRow = `
+  <td>${expense.type}</td>
+  <td>${expense.name}</td>
+  <td>${expense.date}</td>
+  <td>${" $ " + expense.amount}</td>
+  <td>
+  <button id='btn'>Clear Transaction</button>
+  </td>
+  `;
 
-  let deleteBtn = document.createElement("button");
-  deleteBtn.type = "button";
-  deleteBtn.className = "btn";
-  deleteBtn.innerHTML = "Clear Transaction";
-  cell5.appendChild(deleteBtn);
-
-  if (category == "" || type == "" || date == "" || amount == "") {
-    alert("Please fill out all fields");
-  } else {
-    cell2.innerHTML = category;
-    cell1.innerHTML = type;
-    cell3.innerHTML = formattedDate;
-    cell4.innerHTML = `$ ${amount}`;
-
-    deleteBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-
-      row.remove();
-    });
-  }
-
-  const categoryInput = document.getElementById("name");
-  categoryInput.value = "";
-
-  const typeInput = document.getElementById("type");
-  typeInput.value = "";
-
-  const amountInput = document.getElementById("amount");
-  amountInput.value = "";
-
-  const dateInput = document.getElementById("date");
-  dateInput.value = "";
+  table.innerHTML += expenseRow;
 }
 
-function myDeleteFunction() {
-  document.getElementById("transaction-table").deleteRow(0);
-}
+//   let row = tableTransaction.insertRow(-1);
+//   let cell1 = row.insertCell(0);
+//   let cell2 = row.insertCell(1);
+//   let cell3 = row.insertCell(2);
+//   let cell4 = row.insertCell(3);
+//   let cell5 = row.insertCell(4);
+
+//   let deleteBtn = document.createElement("button");
+//   deleteBtn.type = "button";
+//   deleteBtn.className = "btn";
+//   deleteBtn.innerHTML = "Clear Transaction";
+//   cell5.appendChild(deleteBtn);
+
+//   if (category == "" || type == "" || date == "" || amount == "") {
+//     alert("Please fill out all fields");
+//   } else {
+//     cell2.innerHTML = category;
+//     cell1.innerHTML = type;
+//     cell3.innerHTML = formattedDate;
+//     cell4.innerHTML = `$ ${amount}`;
+
+//     deleteBtn.addEventListener("click", (e) => {
+//       e.preventDefault();
+
+//       row.remove();
+//     });
+//   }
+
+//   const categoryInput = document.getElementById("name");
+//   categoryInput.value = "";
+
+//   const typeInput = document.getElementById("type");
+//   typeInput.value = "";
+
+//   const amountInput = document.getElementById("amount");
+//   amountInput.value = "";
+
+//   const dateInput = document.getElementById("date");
+//   dateInput.value = "";
+// }
